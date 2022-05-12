d21d67d28f612328aaec945781f320af7577a307
olivdb <olivdb@protonmail.com>
2022-05-12 14:57:11 +0100

    Restore previous transactions.ts

--- packages/arbitrum-dai-bridge/arbitrum-helpers/transactions.ts
+++ packages/arbitrum-dai-bridge/arbitrum-helpers/transactions.ts
@@ -3,27 +3,15 @@ import { ContractTransaction, providers } from 'ethers'
 export async function waitForTx(
   tx: Promise<ContractTransaction>,
   _confirmations?: number,
-  maxRetries: number = 0,
 ): Promise<providers.TransactionReceipt> {
-  let resolvedTx = null
-
-  for (let retries = 0; retries <= maxRetries; retries++) {
-    try {
-      resolvedTx = await tx
-      break
-    } catch (err) {}
-  }
-  if (!resolvedTx) throw new Error('Max retried exceeded while waiting for resolvedTx in waitForTx()')
-
-  const confirmations = _confirmations ?? chainIdToConfirmationsNeededForFinalization(resolvedTx!.chainId)
+  const resolvedTx = await tx
+  const confirmations = _confirmations ?? chainIdToConfirmationsNeededForFinalization(resolvedTx.chainId)
 
   // we retry .wait b/c sometimes it fails for the first time
-  for (let retries = 0; retries <= maxRetries; retries++) {
-    try {
-      return await resolvedTx!.wait(confirmations)
-    } catch (err) {}
-  }
-  throw new Error('Max retried exceeded while waiting for receipt in waitForTx() ')
+  try {
+    return await resolvedTx.wait(confirmations)
+  } catch (e) {}
+  return await resolvedTx.wait(confirmations)
 }
 
 function chainIdToConfirmationsNeededForFinalization(chainId: number): number {
