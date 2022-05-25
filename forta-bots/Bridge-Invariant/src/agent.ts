e091b193eff9757c85ebba954b624abe1d37fdb5
Lázaro Raúl <riglesiasvera@gmail.com>
2022-05-25 12:11:24 -0400

    feat: Update Bridge Invariant Logic

--- forta-bots/Bridge-Invariant/src/agent.ts
+++ forta-bots/Bridge-Invariant/src/agent.ts
@@ -1,18 +1,19 @@
 import {
   BlockEvent,
   Finding,
-  HandleBlock,
   FindingSeverity,
   FindingType,
+  getEthersProvider,
 } from "forta-agent";
 import { BigNumber, Contract, providers } from "ethers";
 import constants, { NetworkData } from "./constants";
+import SupplyFercher from "./api";
 import abi from "./abi";
 
 const provideHandleBlock = (
   provider: providers.JsonRpcProvider, 
   l2Data: NetworkData[], 
-  fetcher: any, // SupplyFercher,
+  fetcher: SupplyFercher,
   dai: string,
 ) => {
   const daiContract: Contract = new Contract(dai, abi.DAI, provider);
@@ -21,13 +22,13 @@ const provideHandleBlock = (
     const findings: Finding[] = [];
     
     for(let data of l2Data) {
-      const l2Supply: BigNumber = await fetcher.getL2Supply(data.chainId, blockEvent.block.timestamp);
       const escrowSupply: BigNumber = await daiContract.balance(data.escrow, { blockTag: blockEvent.blockNumber });
+      const l2Supply: BigNumber = BigNumber.from(await fetcher.getL2Supply(data.chainId, blockEvent.block.timestamp, escrowSupply));
 
       if(escrowSupply.lt(l2Supply)) {
         findings.push(Finding.from({
           alertId: "MAKER-BRIDGE-INVARIANT",
-          description: "Escrow DAI balance is less that L2 DAI total supply",
+          description: "Escrow DAI balance is less than L2 DAI total supply",
           name: "Maker bridge invariant monitor",
           severity: FindingSeverity.High,
           type: FindingType.Suspicious,
@@ -46,6 +47,10 @@ const provideHandleBlock = (
 };
 
 export default {
-  handleTransaction,
-  // handleBlock
+  handleBlock: provideHandleBlock(
+    getEthersProvider(), 
+    constants.L2_DATA, 
+    new SupplyFercher(),
+    constants.L1_DAI,
+  )
 };
--- forta-bots/Bridge-Invariant/src/constants.ts
+++ forta-bots/Bridge-Invariant/src/constants.ts
@@ -3,6 +3,8 @@ export interface NetworkData {
 	escrow: string,
 } 
 
+const L1_DAI: string = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
+
 const L2_DATA: NetworkData[] = [{
 		chainId: 10, // optimism
 		escrow: "0x467194771dAe2967Aef3ECbEDD3Bf9a310C76C65",
@@ -14,4 +16,5 @@ const L2_DATA: NetworkData[] = [{
 
 export default {
 	L2_DATA,
+	L1_DAI,
 };
