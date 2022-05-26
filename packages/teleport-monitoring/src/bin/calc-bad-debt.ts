b49b9f1026e6d33ad9cb270a2783865f640fc4c2
Krzysztof Kaczor <krzkaczor@gmail.com>
2022-05-26 14:53:06 +0200

    Fix lint

--- packages/teleport-monitoring/src/bin/calc-bad-debt.ts
+++ packages/teleport-monitoring/src/bin/calc-bad-debt.ts
@@ -1,13 +1,4 @@
-import { BigNumber, ethers, providers } from 'ethers'
-
-import { SyncStatusRepository } from '../db/SyncStatusRepository'
-import { TeleportRepository } from '../db/TeleportRepository'
-import { monitorTeleportMints } from '../monitoring/teleportMints'
-import { getL1SdkBasedOnNetworkName, getL2SdkBasedOnNetworkName } from '../sdks'
-import { SyncContext, syncTeleportInits } from '../sync/teleportInits'
 import { calcBadDebt } from '../tasks/calc-bad-debt'
-import { NetworkConfig } from '../types'
-import { delay, inChunks } from '../utils'
 import { run } from './utils'
 
 void run(calcBadDebt)
