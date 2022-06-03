cc01e881c092842a2fae7605c0d9739dcb06a100
Vxatz <vxatzistratis@gmail.com>
2022-06-04 01:17:35 +0300

    feat: Initial commit

---
+++ forta-bots/No-Flushed/src/utils.ts
@@ -0,0 +1,26 @@
+import { Interface } from "@ethersproject/abi";
+import { Finding, FindingSeverity, FindingType } from "forta-agent";
+
+export const DAYS_THRESHOLD: number = 5;
+
+const FLUSHED_EVENT: string = "event Flushed(bytes32 indexed targetDomain, uint256 dai)";
+export const FLUSHED_IFACE: Interface = new Interface([FLUSHED_EVENT]);
+
+export const createFinding = (
+  threshold: number,
+  currentBlockTimestamp: string,
+  latestFlushedTimestamp: string | any = undefined
+): Finding => {
+  return Finding.fromObject({
+    name: "MakerDAO No Flushed monitor",
+    description: `No Flushed event emitted from L2DaiTeleportGateway for ${threshold} days`,
+    alertId: "MK-05",
+    protocol: "MakerDAO",
+    severity: FindingSeverity.Info,
+    type: FindingType.Info,
+    metadata: {
+      currentBlockTimestamp,
+      latestFlushedTimestamp,
+    },
+  });
+};
