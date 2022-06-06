b24c9e667f255ea054ee44758518e9069a7fcd6b
Vxatz <vxatzistratis@gmail.com>
2022-06-06 23:54:05 +0300

    chore: Add missing dependency & change networks names

--- forta-bots/Debt-Ceiling-Utilization/src/network.ts
+++ forta-bots/Debt-Ceiling-Utilization/src/network.ts
@@ -4,20 +4,20 @@ interface NetworkData {
 }
 
 //Optimism
-const KOVAN_TESTNET_DATA: NetworkData = {
+const ETH_KOVAN_TESTNET_DATA: NetworkData = {
   TeleportJoin: "0x556D9076A42Bba1892E3F4cA331daE587185Cef9",
   domains: ["0x4b4f56414e2d534c4156452d4f5054494d49534d2d3100000000000000000000"],
 };
 
 //Arbitrum
-const RINKEBY_TESTNET_DATA: NetworkData = {
+const ETH_RINKEBY_TESTNET_DATA: NetworkData = {
   TeleportJoin: "0x894DB23D804c626f1aAA89a2Bc3280052e6c4750",
   domains: ["0x52494e4b4542592d534c4156452d415242495452554d2d310000000000000000"],
 };
 
 export const NETWORK_MAP: Record<number, NetworkData> = {
-  42: KOVAN_TESTNET_DATA,
-  4: RINKEBY_TESTNET_DATA,
+  42: ETH_KOVAN_TESTNET_DATA,
+  4: ETH_RINKEBY_TESTNET_DATA,
 };
 
 export default class NetworkManager implements NetworkData {
