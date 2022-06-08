10605a55a257220d2ad7b2e7f59c5ffaabf9c44c
Vxatz <vxatzistratis@gmail.com>
2022-06-09 02:18:34 +0300

    chore: Move init logic into handleBlock

--- forta-bots/No-Flushed/src/agent.ts
+++ forta-bots/No-Flushed/src/agent.ts
@@ -6,61 +6,60 @@ import { createFinding, DAYS_THRESHOLD, FLUSHED_IFACE } from "./utils";
 
 const networkManager: NetworkManager = new NetworkManager(NETWORK_MAP);
 
-const botData = {
-  latestFlushedTimestamp: BigNumber.from(0),
-};
-
 export const provideInitialize =
-  (data: NetworkManager, provider: providers.Provider, daysThreshold: number, bData: any): Initialize =>
+  (data: NetworkManager, provider: providers.Provider): Initialize =>
   async () => {
     const { chainId } = await provider.getNetwork();
     data.setNetwork(chainId);
-    const blockNumber: number = await provider.getBlockNumber();
-
-    const filter = {
-      address: data.L2DaiTeleportGateway,
-      topics: [FLUSHED_IFACE.getEventTopic("Flushed")],
-      fromBlock: blockNumber - daysThreshold * 6100, // 6100 blocks/day on average
-      toBlock: blockNumber,
-    };
-
-    const flushedEvents: providers.Log[] = await provider.getLogs(filter);
-
-    if (flushedEvents.length) {
-      bData.latestFlushedTimestamp = BigNumber.from(
-        (await provider.getBlock(flushedEvents[flushedEvents.length - 1].blockNumber)).timestamp
-      );
-    }
   };
 
 export const provideHandleBlock = (
   data: NetworkData,
   provider: providers.Provider,
   daysThreshold: number,
-  bData: any
+  init: boolean
 ): HandleBlock => {
   let latestAlertedFlushedTimestamp: BigNumber = BigNumber.from(-1);
+  let latestFlushedTimestamp: BigNumber = BigNumber.from(0);
   const secondsThreshold: BigNumber = BigNumber.from(daysThreshold).mul(86400);
 
   return async (blockEvent: BlockEvent) => {
     const findings: Finding[] = [];
     const currentTimestamp: BigNumber = BigNumber.from(blockEvent.block.timestamp);
 
+    if (!init) {
+      init = true;
+      const filter = {
+        address: data.L2DaiTeleportGateway,
+        topics: [FLUSHED_IFACE.getEventTopic("Flushed")],
+        fromBlock: blockEvent.block.number - daysThreshold * 6100, // 6100 blocks/day on average
+        toBlock: blockEvent.block.number - 1,
+      };
+
+      const flushedEvents: providers.Log[] = await provider.getLogs(filter);
+
+      if (flushedEvents.length) {
+        latestFlushedTimestamp = BigNumber.from(
+          (await provider.getBlock(flushedEvents[flushedEvents.length - 1].blockNumber)).timestamp
+        );
+      }
+    }
+
     // Check if:
     // 1) Alert has not already been created for this Flushed timestamp
     // 2) Threshold has been exceeded
     if (
-      !latestAlertedFlushedTimestamp.eq(bData.latestFlushedTimestamp) &&
-      currentTimestamp.sub(bData.latestFlushedTimestamp).gt(secondsThreshold)
+      !latestAlertedFlushedTimestamp.eq(latestFlushedTimestamp) &&
+      currentTimestamp.sub(latestFlushedTimestamp).gt(secondsThreshold)
     ) {
       findings.push(
         createFinding(
           daysThreshold,
           currentTimestamp.toString(),
-          !bData.latestFlushedTimestamp.eq(0) ? bData.latestFlushedTimestamp.toString() : undefined
+          !latestFlushedTimestamp.eq(0) ? latestFlushedTimestamp.toString() : undefined
         )
       );
-      latestAlertedFlushedTimestamp = bData.latestFlushedTimestamp;
+      latestAlertedFlushedTimestamp = latestFlushedTimestamp;
     }
     const filter = {
       address: data.L2DaiTeleportGateway,
@@ -70,7 +69,7 @@ export const provideHandleBlock = (
     const flushedEvents: providers.Log[] = await provider.getLogs(filter);
 
     if (flushedEvents.length) {
-      bData.latestFlushedTimestamp = BigNumber.from(blockEvent.block.timestamp);
+      latestFlushedTimestamp = BigNumber.from(blockEvent.block.timestamp);
     }
 
     return findings;
@@ -78,6 +77,6 @@ export const provideHandleBlock = (
 };
 
 export default {
-  initialize: provideInitialize(networkManager, getEthersProvider(), DAYS_THRESHOLD, botData),
-  handleBlock: provideHandleBlock(networkManager, getEthersProvider(), DAYS_THRESHOLD, botData),
+  initialize: provideInitialize(networkManager, getEthersProvider()),
+  handleBlock: provideHandleBlock(networkManager, getEthersProvider(), DAYS_THRESHOLD, false),
 };
