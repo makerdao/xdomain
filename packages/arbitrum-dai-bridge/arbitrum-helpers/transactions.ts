f56b8c8a6bdd296f6e6b552ab707d8dc65c831a5
olivdb <olivdb@protonmail.com>
2022-08-13 14:44:27 +0100

    Fix arbitrum-dai-bridge tests

--- packages/arbitrum-dai-bridge/arbitrum-helpers/transactions.ts
+++ packages/arbitrum-dai-bridge/arbitrum-helpers/transactions.ts
@@ -1,4 +1,5 @@
 import { ContractTransaction, providers } from 'ethers'
+import { sleep } from '@eth-optimism/core-utils'
 
 export async function waitForTx(
   tx: Promise<ContractTransaction>,
@@ -8,10 +9,20 @@ export async function waitForTx(
   const confirmations = _confirmations ?? chainIdToConfirmationsNeededForFinalization(resolvedTx.chainId)
 
   // we retry .wait b/c sometimes it fails for the first time
-  try {
-    return await resolvedTx.wait(confirmations)
-  } catch (e) {}
-  return await resolvedTx.wait(confirmations)
+  for (let attempts = 1; attempts <= 12; attempts++) {
+    try {
+      const txReceipt = await resolvedTx.wait(confirmations)
+      if (txReceipt) return txReceipt
+      else console.log(`Transaction .wait() returned ${txReceipt}`)
+    } catch (e) {
+      console.log(`Transaction .wait() error: ${e}`)
+    }
+    console.log(`Retrying in 10s...`)
+    await sleep(10000)
+  }
+  throw new Error(
+    `Transaction .wait(${confirmations}) didn't succeed after several attempts. Transaction: ${resolvedTx}`,
+  )
 }
 
 function chainIdToConfirmationsNeededForFinalization(chainId: number): number {
