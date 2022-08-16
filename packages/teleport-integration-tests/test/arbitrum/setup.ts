aad945fd9a856c118e92e899306b7d79be4a4485
olivdb <olivdb@protonmail.com>
2022-08-16 21:05:09 +0100

    Try fix integr tests

--- packages/teleport-integration-tests/test/arbitrum/setup.ts
+++ packages/teleport-integration-tests/test/arbitrum/setup.ts
@@ -8,9 +8,8 @@ import {
   getMaxSubmissionPrice_Nitro,
   waitToRelayTxsToL2_Nitro,
 } from 'arbitrum-dai-bridge'
-import { ContractReceipt, ContractTransaction, Wallet } from 'ethers'
-import { formatEther } from 'ethers/lib/utils'
-import { ethers } from 'hardhat'
+import { ContractReceipt, ContractTransaction, providers,Wallet } from 'ethers'
+import { formatEther, parseEther } from 'ethers/lib/utils'
 
 import { L1AddTeleportArbitrumSpell__factory, L2AddTeleportDomainSpell__factory } from '../../typechain'
 import { deployUsingFactory, getContractFactory, waitForTx } from '../helpers'
@@ -34,16 +33,16 @@ export async function setupArbitrumTests({
   const pkey = getRequiredEnv('DEPLOYER_PRIV_KEY')
   const pkey2 = getOptionalEnv('USER_PRIV_KEY')
 
-  const l1Provider = new ethers.providers.JsonRpcProvider(l1Rpc)
+  const l1Provider = new providers.JsonRpcProvider(l1Rpc)
   const l2Provider = new RetryProvider(50, l2Rpc)
   const l1StartingBlock = await l1Provider.getBlockNumber()
   const l2StartingBlock = await l2Provider.getBlockNumber()
   console.log('Current L1 block: ', l1StartingBlock)
   console.log('Current L2 block: ', l2StartingBlock)
 
-  const l1Signer = new ethers.Wallet(pkey, l1Provider)
-  const l2Signer = new ethers.Wallet(pkey, l2Provider)
-  const l1User = pkey2 ? new ethers.Wallet(pkey2, l1Provider) : Wallet.createRandom().connect(l1Provider)
+  const l1Signer = new Wallet(pkey, l1Provider)
+  const l2Signer = new Wallet(pkey, l2Provider)
+  const l1User = pkey2 ? new Wallet(pkey2, l1Provider) : Wallet.createRandom().connect(l1Provider)
   const l2User = l1User.connect(l2Provider)
   console.log('l1Signer:', l1Signer.address)
   console.log('l1User:', l1User.address)
@@ -52,7 +51,7 @@ export async function setupArbitrumTests({
   const makerSdk = l1Sdk.maker
   const arbitrumRollupSdk = l1Sdk.arbitrum
 
-  const userEthAmount = ethers.utils.parseEther('0.1')
+  const userEthAmount = parseEther('0.1')
   if ((await l1User.getBalance()).lt(userEthAmount)) {
     console.log('Funding l1User ETH balance...')
     await (await l1Signer.sendTransaction({ to: l1User.address, value: userEthAmount })).wait()
