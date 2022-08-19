60963324c543cae4691c19203e799a8a08cceb44
olivdb <olivdb@protonmail.com>
2022-08-19 19:35:52 +0100

    Disable strict mode

--- packages/teleport-example-ui/src/index.tsx
+++ packages/teleport-example-ui/src/index.tsx
@@ -8,9 +8,7 @@ import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
 )
