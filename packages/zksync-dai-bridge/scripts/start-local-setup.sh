6d0f361335da740c450a91cc40efe4e2e3bbd49e
olivdb <olivdb@protonmail.com>
2022-08-26 18:24:37 +0100

    Clear local setup upon start

--- packages/zksync-dai-bridge/scripts/start-local-setup.sh
+++ packages/zksync-dai-bridge/scripts/start-local-setup.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 git clone https://github.com/matter-labs/local-setup.git
 cd local-setup
+git checkout 65f57fe7e266438e9c2b53e4f61ec253e85cd014
 
+rm -rf ./volumes
 mkdir -p ./volumes
 mkdir -p ./volumes/postgres ./volumes/geth ./volumes/zksync/env/dev ./volumes/zksync/data
 
