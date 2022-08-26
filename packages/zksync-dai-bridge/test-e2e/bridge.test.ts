51aea777ed961bac6b49db0bbf4c2b57df592dce
olivdb <olivdb@protonmail.com>
2022-08-26 21:43:21 +0100

    Add first working l1>l2 deposit e2e test

--- packages/zksync-dai-bridge/test-e2e/bridge.test.ts
+++ packages/zksync-dai-bridge/test-e2e/bridge.test.ts
@@ -1,14 +1,16 @@
 import fs from 'fs'
 import { expect } from 'chai'
 import * as zk from 'zksync-web3'
+import { IZkSync } from 'zksync-web3/build/typechain'
 import { ethers } from 'hardhat'
 import * as hre from 'hardhat'
-import { Wallet, Contract } from 'ethers'
+import { BigNumber, Wallet, Contract } from 'ethers'
 import { Deployer } from '@matterlabs/hardhat-zksync-deploy'
 
 import { L1DAITokenBridge, L1Escrow, L1GovernanceRelay } from '../typechain-types/l1'
 import { Dai, L2DAITokenBridge, L2GovernanceRelay, TestBridgeUpgradeSpell } from '../typechain-types/l2'
 import { getActiveWards, getAddressOfNextDeployedContract, waitForTx } from '@makerdao/hardhat-utils'
+import { parseEther } from 'ethers/lib/utils'
 
 const RICH_WALLET_PK = '0x7726827caac94a7f9e1b160f7ea819f172f7b6f9d2a97f992c38edeab82d4110'
 const depositAmount = ethers.utils.parseEther('5')
@@ -43,7 +45,25 @@ async function deployL1Contract<T extends Contract>(
   return contract as T
 }
 
+async function setupSigners(): Promise<{
+  l1Signer: Wallet
+  l2Signer: zk.Wallet
+}> {
+  const l1Provider = new ethers.providers.JsonRpcProvider(hre.config.zkSyncDeploy.ethNetwork)
+  const l2Provider = new zk.Provider(hre.config.zkSyncDeploy.zkSyncNetwork)
+
+  const privKey = process.env.TEST_ENV === 'goerli' ? process.env.GOERLI_DEPLOYER_PRIV_KEY : RICH_WALLET_PK
+  if (!privKey) throw new Error(`Missing GOERLI_DEPLOYER_PRIV_KEY env var`)
+
+  const l1Signer = new Wallet(privKey, l1Provider)
+  const l2Signer = new zk.Wallet(privKey, l2Provider, l1Provider)
+
+  console.log(`Deployer address: ${l1Signer.address}`)
+  return { l1Signer, l2Signer }
+}
+
 describe('bridge', function () {
+  let zkSync: IZkSync
   let l1Signer: Wallet
   let l2Signer: zk.Wallet
 
@@ -58,8 +78,9 @@ describe('bridge', function () {
   //   let l2DAITokenBridgeV2: L2DAITokenBridge
   let l2GovernanceRelay: L2GovernanceRelay
   beforeEach(async () => {
-    ;({ l1Signer, l2Signer } = await setupTest())
+    ;({ l1Signer, l2Signer } = await setupSigners())
     const zkSyncAddress = await l2Signer.provider.getMainContractAddress()
+    zkSync = new Contract(zkSyncAddress, zk.utils.ZKSYNC_MAIN_ABI, l1Signer) as IZkSync
 
     l2Dai = await deployL2Contract(l2Signer, 'Dai')
     l1Dai = await deployL1Contract(l1Signer, 'L1Dai')
@@ -97,6 +118,10 @@ describe('bridge', function () {
       'Predicted address of l1GovernanceRelay doesnt match actual address',
     )
 
+    console.log('Minting deployer Dai...')
+    await waitForTx(l1Dai.mint(l1Signer.address, parseEther('1000000'), { gasLimit: 200000 }))
+    console.log('Approving use of deployer Dai by l1DAITokenBridge...')
+    await waitForTx(l1Dai.approve(l1DAITokenBridge.address, ethers.constants.MaxUint256, { gasLimit: 200000 }))
     console.log('Approving l1DAITokenBridge to move l1Dai from L1Escrow...')
     await waitForTx(
       l1Escrow.approve(l1Dai.address, l1DAITokenBridge.address, ethers.constants.MaxUint256, { gasLimit: 200000 }),
@@ -118,22 +143,33 @@ describe('bridge', function () {
 
   it('moves l1 tokens to l2', async function () {
     await waitForTx(l1Dai.approve(l1DAITokenBridge.address, depositAmount, { gasLimit: 200000 }))
+    const l2DaiBefore = await l2Dai.balanceOf(l2Signer.address)
+    const l2Calldata = l2DAITokenBridge.interface.encodeFunctionData('finalizeDeposit', [
+      l1Signer.address,
+      l2Signer.address,
+      l1Dai.address,
+      depositAmount,
+      '0x',
+    ])
+    const gasPrice = await l1Signer.provider.getGasPrice()
+    const ergsLimit = BigNumber.from(1000000)
+    const queueType = 0
+    const l2ExecutionCost = await zkSync.l2TransactionBaseCost(
+      gasPrice,
+      ergsLimit,
+      ethers.utils.hexlify(l2Calldata).length,
+      queueType,
+    )
+    const tx = await l1DAITokenBridge.deposit(l2Signer.address, l1Dai.address, depositAmount, queueType, {
+      value: l2ExecutionCost,
+      gasPrice,
+      gasLimit: 300000,
+    })
+    await tx.wait()
+    const l2Response = await l2Signer.provider.getL2TransactionFromPriorityOp(tx)
+    await l2Response.wait()
+
+    const l2DaiAfter = await l2Dai.balanceOf(l2Signer.address)
+    expect(l2DaiAfter.sub(l2DaiBefore).toString()).to.be.eq(depositAmount.toString())
   })
 })
-
-export async function setupTest(): Promise<{
-  l1Signer: Wallet
-  l2Signer: zk.Wallet
-}> {
-  const l1Provider = new ethers.providers.JsonRpcProvider(hre.config.zkSyncDeploy.ethNetwork)
-  const l2Provider = new zk.Provider(hre.config.zkSyncDeploy.zkSyncNetwork)
-
-  const privKey = process.env.TEST_ENV === 'goerli' ? process.env.GOERLI_DEPLOYER_PRIV_KEY : RICH_WALLET_PK
-  if (!privKey) throw new Error(`Missing GOERLI_DEPLOYER_PRIV_KEY env var`)
-
-  const l1Signer = new Wallet(privKey, l1Provider)
-  const l2Signer = new zk.Wallet(privKey, l2Provider, l1Provider)
-
-  console.log(`Deployer address: ${l1Signer.address}`)
-  return { l1Signer, l2Signer }
-}
