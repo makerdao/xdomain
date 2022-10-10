94bfd952016940d732179a9c30ec961634122b2f
olivdb <olivdb@protonmail.com>
2022-10-10 14:20:58 +0100

    Update teleport-keeper domains and dependencies

--- packages/teleport-keeper/src/config.ts
+++ packages/teleport-keeper/src/config.ts
@@ -3,30 +3,46 @@ import { invert } from 'lodash'
 import { makeFinalizeMessageForArbitrum, makeFinalizeMessageForOptimism } from './domains'
 
 export const chainIds = {
-  KOVAN: 42,
-  RINKEBY: 4,
+  GOERLI: 5,
+  MAINNET: 1,
 }
 export const idsToChains = invert(chainIds)
 
 const DAY = 60 * 60 * 24
 export const networks = {
-  [chainIds.KOVAN]: [
+  [chainIds.GOERLI]: [
     {
-      name: 'KOVAN-SLAVE-OPTIMISM-1',
-      l2Rpc: 'https://kovan.optimism.io/',
-      domainsToFlush: ['KOVAN-MASTER-1'],
+      name: 'OPT-GOER-A',
+      l2Rpc: 'https://goerli.optimism.io/',
+      domainsToFlush: ['ETH-GOER-A'],
       maxTtlForMessages: 9 * DAY,
-      teleportOutboundGateway: '0x0aeDbEf4105fdfc0db5A3Cd8C827bE2efA93ebe0',
+      teleportOutboundGateway: '0xd9e000C419F3aA4EA1C519497f5aF249b496a00f',
       messageFinalizer: makeFinalizeMessageForOptimism,
     },
-  ],
-  [chainIds.RINKEBY]: [
     {
-      name: 'RINKEBY-SLAVE-ARBITRUM-1',
-      l2Rpc: 'https://rinkeby.arbitrum.io/rpc',
-      domainsToFlush: ['RINKEBY-MASTER-1'],
+      name: 'ARB-GOER-A',
+      l2Rpc: 'https://goerli-rollup.arbitrum.io/rpc',
+      domainsToFlush: ['ETH-GOER-A'],
       maxTtlForMessages: 9 * DAY,
-      teleportOutboundGateway: '0x327c2f7aCd799f31535880Af54C2bCAB1384Ecc3',
+      teleportOutboundGateway: '0x8334a747731Be3a58bCcAf9a3D35EbC968806223',
+      messageFinalizer: makeFinalizeMessageForArbitrum,
+    },
+  ],
+  [chainIds.MAINNET]: [
+    {
+      name: 'OPT-MAIN-A',
+      l2Rpc: 'https://mainnet.optimism.io/',
+      domainsToFlush: ['ETH-MAIN-A'],
+      maxTtlForMessages: 9 * DAY,
+      teleportOutboundGateway: '0x18d2CF2296c5b29343755E6B7e37679818913f88',
+      messageFinalizer: makeFinalizeMessageForOptimism,
+    },
+    {
+      name: 'ARB-ONE-A',
+      l2Rpc: 'https://arb1.arbitrum.io/rpc',
+      domainsToFlush: ['ETH-MAIN-A'],
+      maxTtlForMessages: 9 * DAY,
+      teleportOutboundGateway: '0x5dBaf6F2bEDebd414F8d78d13499222347e59D5E',
       messageFinalizer: makeFinalizeMessageForArbitrum,
     },
   ],
