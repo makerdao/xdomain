5162daaefd217415032a3ae5aec598bc645caa7f
olivdb <olivdb@protonmail.com>
2023-01-16 15:44:57 +0000

    Adjust formatting to previous bridges

--- packages/zksync-dai-bridge/contracts/l2/test/TestBridgeUpgradeSpell.sol
+++ packages/zksync-dai-bridge/contracts/l2/test/TestBridgeUpgradeSpell.sol
@@ -3,28 +3,28 @@
 pragma solidity >=0.7.6;
 
 interface BridgeLike {
-    function close() external;
+  function close() external;
 
-    function l2Token() external view returns (address);
+  function l2Token() external view returns (address);
 }
 
 interface AuthLike {
-    function rely(address usr) external;
+  function rely(address usr) external;
 
-    function deny(address usr) external;
+  function deny(address usr) external;
 }
 
 /**
  * An example spell to transfer from the old bridge to the new one.
  */
 contract TestBridgeUpgradeSpell {
-    function upgradeBridge(address _oldBridge, address _newBridge) external {
-        BridgeLike oldBridge = BridgeLike(_oldBridge);
-        AuthLike dai = AuthLike(oldBridge.l2Token());
+  function upgradeBridge(address _oldBridge, address _newBridge) external {
+    BridgeLike oldBridge = BridgeLike(_oldBridge);
+    AuthLike dai = AuthLike(oldBridge.l2Token());
 
-        oldBridge.close();
-        // note: ususally you wouldn't "deny" right away b/c of async messages
-        dai.deny(_oldBridge);
-        dai.rely(_newBridge);
-    }
+    oldBridge.close();
+    // note: ususally you wouldn't "deny" right away b/c of async messages
+    dai.deny(_oldBridge);
+    dai.rely(_newBridge);
+  }
 }
--- packages/zksync-dai-bridge/contracts/l2/test/TestDaiMintSpell.sol
+++ packages/zksync-dai-bridge/contracts/l2/test/TestDaiMintSpell.sol
@@ -3,18 +3,14 @@
 pragma solidity >=0.7.6;
 
 interface MintLike {
-    function mint(address to, uint256 value) external;
+  function mint(address to, uint256 value) external;
 }
 
 /**
  * An example spell to mint some dai.
  */
 contract TestDaiMintSpell {
-    function mintDai(
-        address _dai,
-        address _user,
-        uint256 _amount
-    ) external {
-        MintLike(_dai).mint(_user, _amount);
-    }
+  function mintDai(address _dai, address _user, uint256 _amount) external {
+    MintLike(_dai).mint(_user, _amount);
+  }
 }
