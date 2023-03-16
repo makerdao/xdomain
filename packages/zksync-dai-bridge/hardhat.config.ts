e6653413f23d6a568dced1618582d8a77ff2e760
olivdb <olivdb@protonmail.com>
2023-03-16 20:12:11 +0000

    Update zksolc

--- packages/zksync-dai-bridge/hardhat.config.ts
+++ packages/zksync-dai-bridge/hardhat.config.ts
@@ -27,12 +27,12 @@ const zkSyncDeploy =
 
 const config: HardhatUserConfig = {
   zksolc: {
-    version: '1.3.5',
+    version: '1.3.7',
     compilerSource: 'binary',
     settings: {
       experimental: {
         dockerImage: 'matterlabs/zksolc',
-        tag: 'v1.3.5',
+        tag: 'v1.3.7',
       },
     },
   },
